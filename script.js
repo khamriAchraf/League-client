3997b8170268a5c0c4f36dfa974ec5d68e188292
Achraf Khamri <achrafkhamri99@gmail.com>
2021-05-01 14:14:32 +0000

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function openPartyOver(){
 function openPartyOut(){
     document.querySelector(".open-party .join").classList.remove("join-visible");
 }
+
+
+function muteVideo(){
+    document.querySelector(".video").muted=true;
+}
+
+function unmuteVideo(){
+    document.querySelector(".video").muted=false;
+}
+
+function myFunction() {
+    var checkBox=document.querySelector("#stopAudio");
+    if (checkBox.checked==true){
+        muteVideo();
+    } else {
+        unmuteVideo();
+    }
+}
+
+function myOtherFunction() {
+    var checkBox=document.querySelector("#stopVideo");
+    if (checkBox.checked==true){
+        document.querySelector(".player").innerHTML="<audio class=\"video\" autoplay><source src=\"img/login.mp3\" type=\"audio/mpeg\"></audio>";
+        document.querySelector(".video").play();
+    } else {
+        document.querySelector(".player").innerHTML="<video class= \"img-fluid video\" autoplay loop id=\"myVideo\"><source class=\"\" src=\"img/login.mp4\" type=\"video/mp4\"></video>";
+        document.querySelector(".video").play();
+    }
+}
