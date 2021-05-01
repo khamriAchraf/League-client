3877ca576d30f9b28858170a175b04029c1c320f
Achraf Khamri <achrafkhamri99@gmail.com>
2021-05-01 11:26:26 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPartyOver(){
+    document.querySelector(".open-party .join").classList.add("join-visible");
+}
+
+function openPartyOut(){
+    document.querySelector(".open-party .join").classList.remove("join-visible");
+}
