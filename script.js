c1f982d0f926ddeeb2241e2437a38892e3a43e15
Achraf Khamri <achrafkhamri99@gmail.com>
2021-05-02 12:20:44 +0000

    third commit

--- script.js
+++ script.js
@@ -26,11 +26,25 @@ function myFunction() {
 
 function myOtherFunction() {
     var checkBox=document.querySelector("#stopVideo");
+    var checkBox2=document.querySelector("#stopAudio");
     if (checkBox.checked==true){
         document.querySelector(".player").innerHTML="<audio class=\"video\" autoplay><source src=\"img/login.mp3\" type=\"audio/mpeg\"></audio>";
         document.querySelector(".video").play();
+        if (checkBox2==true){
+            muteVideo();
+        }
     } else {
         document.querySelector(".player").innerHTML="<video class= \"img-fluid video\" autoplay loop id=\"myVideo\"><source class=\"\" src=\"img/login.mp4\" type=\"video/mp4\"></video>";
         document.querySelector(".video").play();
+        if (checkBox2==true){
+            muteVideo();
+        }
     }
+}
+
+function submit(){
+    const formData = new FormData(document.querySelector('form'))
+for (var pair of formData.entries()) {
+  console.log(pair[0] + ': ' + pair[1]);
+}
 }
